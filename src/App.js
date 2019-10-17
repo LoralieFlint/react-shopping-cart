99a55566d1f9a0518cf48d35adabce914eeea741
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-10-16 18:16:55 -0600

    CartContect works and removeItem stretch task complete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
 //context API
-import ProductContext from "../src/contexts/ProductContext";
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-		setCart([...cart, item])
-	};
+  const addItem = item => {
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-	const removeItem = del => {
-		setCart(cart.filter(item => {
-			return item.del !== del;
-		}))
-	}
+  const removeItem = id => {
+    setCart(
+      cart.filter(item => {
+        return item.id !== id;
+      })
+    );
+  };
 
-	return (
-		<ProductContext.Provider value={{ products, addItem }}>
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/" component={Products} />
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-		</ProductContext.Provider>
-
-	);
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <div className="App">
+          <Navigation />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />} />
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
